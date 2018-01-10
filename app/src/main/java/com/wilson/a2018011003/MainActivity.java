1449b75bddb519ba01ba9420991b2340eaa53744
C1 <c1@gjun.com>
2018-01-10 13:42:26 +0800

    Initial commit

---
+++ app/src/main/java/com/wilson/a2018011003/MainActivity.java
@@ -0,0 +1,36 @@
+package com.wilson.a2018011003;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Log.d("MSG", "取得一個 Message");
+            Bundle b = msg.getData();
+            String str = b.getString("data");
+            Log.d("MSG", "data:" + str);
+        }
+    };
+    public void click1(View v)
+    {
+        Message msg = new Message();
+        Bundle b = new Bundle();
+        b.putString("data", "MyData");
+        msg.setData(b);
+        handler.sendMessageDelayed(msg, 3000);
+    }
+}
+
